a0a6214850832c3c7fe8f2adcab5a7a688966b5a
HubTakenIs <hubertaslol@gmail.com>
2026-09-21 22:53:41 +0100

    experimenting

--- datahelper.py
+++ datahelper.py
@@ -1,24 +1,21 @@
 import os
+import click
 
-class DataHelper():
-    def __init__(self):
-        pass
+@click.command()
+@click.option("--path", default="./data", help="path to data directory.")
+def setup_data_folder(path):
+    data_dir_path = os.path.abspath(path)
+    if os.path.exists(data_dir_path) and os.path.isdir(data_dir_path):
+        print(f"Data folder is already set up, {data_dir_path}")
+    else:
+        print(f"Data folder needs setting up.")
+        print(f"Will create data folder")
+        os.mkdir(data_dir_path)
     
-    def load_data():
-        pass
-    def write_data():
-        pass
+def setup_data_file(self, file_name):
+    data_dir_path = os.path.abspath("./data/")
+    if os.path.exists(data_dir_path):
+        f = open(file_name, "x")
 
-    def setup_data_folder(self):
-        data_dir_path = os.path.abspath("./data/")
-        if os.path.exists(data_dir_path) and os.path.isdir(data_dir_path):
-            print(f"Data folder is already set up, {data_dir_path}")
-        else:
-            print(f"Data folder needs setting up.")
-            print(f"Will create data folder")
-            os.mkdir(data_dir_path)
-    def setup_data_file(self, file_name):
-        data_dir_path = os.path.abspath("./data/")
-        if os.path.exists(data_dir_path):
-            f = open(file_name, "x")
-
+if __name__ == "__main__":
+    setup_data_folder(
--- tests/test_datahelper.py
+++ tests/test_datahelper.py
@@ -1,15 +1,14 @@
 import unittest
-from datahelper import DataHelper
+from datahelper import setup_data_folder
 import os
 
 class setup_tests(unittest.TestCase):
-    
+    @unittest.skip("Testing skip")
     def test_setup_data_folder(self):
-       datahelper = DataHelper()
        abs_path = os.path.abspath("./data/")
        before_setup = os.path.exists(abs_path)
        self.assertFalse(before_setup)
-       datahelper.setup_data_folder()
+       setup_data_folder()
        after_setup = os.path.exists(abs_path) and os.path.isdir(abs_path)
        self.assertTrue(after_setup)
 
