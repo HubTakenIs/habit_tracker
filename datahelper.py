9a407e43228655680ba99d84dca072b8de0f89c2
HubTakenIs <hubertaslol@gmail.com>
2026-09-22 23:55:29 +0100

    made progress towards first priority

--- datahelper.py
+++ datahelper.py
@@ -1,5 +1,6 @@
 import os
 import click
+import datetime
 
 @click.command()
 @click.option("--path", default="./data", help="path to data directory.")
@@ -11,11 +12,28 @@ def setup_data_folder(path):
         print(f"Data folder needs setting up.")
         print(f"Will create data folder")
         os.mkdir(data_dir_path)
-    
+
+def list_of_dates():
+    num_of_dates = 365
+    start = datetime.datetime(2026,1,1)
+    date_list = []
+    for x in range(num_of_dates):
+        date_list.append(start.date() + datetime.timedelta(days=x))
+    print(date_list)
+    return date_list
+
+
+
 def setup_data_file(self, file_name):
     data_dir_path = os.path.abspath("./data/")
     if os.path.exists(data_dir_path):
         f = open(file_name, "x")
+        f.close()
+    f = open(file_name, "a")
+    f.write("Date, Ticked")
+    
 
 if __name__ == "__main__":
-    setup_data_folder(
+    ml = list_of_dates()
+    for item in ml:
+        print(item)
